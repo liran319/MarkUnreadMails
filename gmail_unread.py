06e252ee053e86e492463f1da5df0c1c461065c4
Ran <liran319@gmail.com>
2014-09-11 17:38:29 +0800

    修改登录逻辑，如果login失败，会提示继续登录

--- gmail_unread.py
+++ gmail_unread.py
@@ -1,19 +1,39 @@
 # -*- conding: utf-8 -*-
 
 import imaplib
+import getpass
 # import sys
 
-USERNAME = raw_input('Please input your email address: ')
-PASSWORD = raw_input('Please input your passward: ')
 M = imaplib.IMAP4_SSL('imap.gmail.com')
-print M.login(USERNAME, PASSWORD)
-M.list()
-M.select('inbox')
-typ, data = M.search(None, 'UNSEEN')
-for num in data[0].split():
-    typ, data = M.fetch(num, '(BODY.PEEK[])')
-    M.store(num, '+FLAGS', '\Seen')
-    # print 'Message %s\n%s\n' % (num, repr(data))
-print "Done!"
-M.close()
-M.logout()
+
+
+def account_login():
+
+    USERNAME = raw_input('Please input your email address: ')
+    PASSWORD = getpass.getpass('Please input your passward: ')
+    try:
+        status = M.login(USERNAME, PASSWORD)
+    except Exception as e:
+        print e
+        status = ['Fail']
+    print status
+    if status[0] == 'OK':
+        return True
+    else:
+        return False
+
+while 1:
+    if account_login():
+        M.list()
+        M.select('inbox')
+        typ, data = M.search(None, 'UNSEEN')
+        for num in data[0].split():
+            typ, data = M.fetch(num, '(BODY.PEEK[])')
+            M.store(num, '+FLAGS', '\Seen')
+            print 'Message %s\n%s\n' % (num, repr(data))
+        print "Done!"
+        M.close()
+        M.logout()
+        break
+    else:
+        pass
